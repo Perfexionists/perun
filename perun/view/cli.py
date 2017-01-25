a584d7be0e513f444c61759c83bdc590155a74ec
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2017-01-25 21:40:31 +0100

    Init dummy functions of the cli.

--- perun/view/cli.py
+++ perun/view/cli.py
@@ -7,5 +7,36 @@ __author__ = 'Tomas Fiedor'
 def cli():
     print("Hello World! From perun")
 
+
+@cli.command()
+def config():
+    print("perun config run")
+
+
+@cli.command()
+def init():
+    print("perun init run")
+
+
+@cli.command()
+def add():
+    print("perun add run")
+
+
+@cli.command()
+def rm():
+    print("perun rm run")
+
+
+@cli.command()
+def log():
+    print("perun log run")
+
+
+@cli.command()
+def show():
+    print("perun show run")
+
+
 if __name__ == "__main__":
     cli()
