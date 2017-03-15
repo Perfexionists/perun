532e687bfd25c61b99720670092031d781860b1c
Jiří Pavela <xpavel32@stud.fit.vutbr.cz>
2017-03-15 16:44:15 +0100

    Move the complexity collector exception to the perun utils exceptions.
    complexity_exceptions.py: the complexity collector spefific exception
    exceptions.py: added the complexity collector exception
    complexity.py, symbols.py: changed the import paths

--- perun/collect/complexity/complexity_exceptions.py
+++
@@ -1,8 +0,0 @@
-""" The exception module for complexity collector.
-
-"""
-
-
-class UnexpectedPrototypeSyntaxError(Exception):
-    """ Raised when the function prototype syntax is somehow different than expected """
-    pass
