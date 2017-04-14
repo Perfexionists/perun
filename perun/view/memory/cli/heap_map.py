a7554f6e2832c8d7286c44c68feb6e5fb8b50887
Radim Podola <RPodola@seznam.cz>
2017-04-15 01:48:46 +0200

    Add a HEAT feature to the HEAP map visualization. heap_representation: Minor changes in the structure format.

--- perun/view/memory/cli/heap_map.py
+++ perun/view/memory/cli/heap_map.py
@@ -79,7 +79,7 @@ class HeapMapColors(object):
         curses.init_pair(start_pair_number, -1, 16)
         self.info_text = start_pair_number
 
-    def get_field_color(self, uid):
+    def heap_field_color(self, uid):
         """ Pick a right color for the given field.
 
         Arguments:
@@ -101,6 +101,13 @@ class HeapMapColors(object):
 
         return color
 
+    def heat_field_color(self, access):
+        if access == 0:
+            return 1
+        elif access == 1:
+            return 2
+        else:
+            return 3
 
 class HeapMapVisualization(object):
     """ Class providing visualization of the heap map.
@@ -164,10 +171,11 @@ class HeapMapVisualization(object):
 
     # text's constants
     __ADDRESS_INFO_TEXT = 'ADDRESS:'
-    __MENU_TEXT = '[Q] QUIT  [<] PREVIOUS  [>] NEXT  [A] ANIMATE  ' \
-                  '[4|8|6|5] CURSOR L|U|R|D'
+    __MENU_TEXT = '[Q] QUIT [<] PREV [>] NEXT ' \
+                  '[4|8|6|5] CURSOR MOVE [A] ANIMATE [H] HEAT'
     __ANIME_MENU_TEXT = '[S] STOP  [P] PAUSE  [R] RESTART'
     __ANIME_CONTINUE_TEXT = '[C] CONTINUE'
+    __HEAT_MENU_TEXT = '[Q] QUIT [4|8|6|5] CURSOR MOVE'
     __RESIZE_REQ_TEXT = "Increase the size of your screen, please"
 
     def show_intro(self):
@@ -216,7 +224,7 @@ class HeapMapVisualization(object):
             margin(int): left margin
         """
         text = 'SNAPSHOT: ' + str(self.__current_snap)
-        text += '/' + str(self.__heap['max']) + '  ('
+        text += '/' + str(len(self.__heap['snapshots'])) + '  ('
         text += str(self.__heap['snapshots'][self.__current_snap - 1]['time'])
         text += 's)'
         row_pos = 0
@@ -263,6 +271,23 @@ class HeapMapVisualization(object):
         # set blocking window.getch()
         self.__window.nodelay(0)
 
+    def __calculate_field_size(self, rows, cols):
+        """ Calculate the field size based on the screen's size
+
+        Arguments:
+            rows(int): total number of the screen's rows
+            cols(int): total number of the screen's columns
+        
+        Returns:
+            float: field size
+        """
+        stats = self.__heap['stats']
+
+        # calculating approximated field size
+        line_address_range = stats['max_address'] - stats['min_address']
+
+        return line_address_range / (rows * cols)
+
     def create_screen_decomposition(self, rows, cols):
         """ Create a matrix with corresponding representation of the snapshot
 
@@ -275,11 +300,8 @@ class HeapMapVisualization(object):
                   and size of the field
         """
         snap = self.__heap['snapshots'][self.__current_snap - 1]
-        stats = self.__heap['stats']
 
-        # calculating approximated field size
-        line_address_range = stats['max_address'] - stats['min_address']
-        field_size = line_address_range / (rows * cols)
+        field_size = self.__calculate_field_size(rows, cols)
 
         # creating empty matrix
         matrix = [[None for _ in range(cols)] for _ in range(rows)]
@@ -291,7 +313,7 @@ class HeapMapVisualization(object):
 
         record = next(iterator, None)
         # set starting address to first approx field
-        last_field = stats['min_address']
+        last_field = self.__heap['stats']['min_address']
         remain_amount = 0 if record is None else record['amount']
         for row in range(rows):
             for col in range(cols):
@@ -367,7 +389,10 @@ class HeapMapVisualization(object):
                 # field printing
                 else:
                     field = map_data['data'][row - 1][col - add_length - 1]
-                    uid = field['uid']
+                    if self.__heap['type'] == "heap":
+                        color = self.__colors.heap_field_color(field['uid'])
+                    else:
+                        color = self.__colors.heat_field_color(field['access'])
 
                     if tik_counter % self.TIK_FREQ == 0:
                         symbol = self.TIK_SYM
@@ -376,7 +401,6 @@ class HeapMapVisualization(object):
                     else:
                         symbol = self.FIELD_SYM
 
-                    color = self.__colors.get_field_color(uid)
                     self.__window.addstr(row, col, symbol,
                                          curses.color_pair(color))
 
@@ -430,37 +454,28 @@ class HeapMapVisualization(object):
         """
         curses.update_lines_cols()
         self.__window.clear()
-        self.__map_cords = {}
+        self.__map_cords = None
 
-        # calculate space for the addresses information
-        max_add_len = len(str(self.__heap['stats']['max_address']))
-        if max_add_len < len(self.__ADDRESS_INFO_TEXT):
-            max_add_len = len(self.__ADDRESS_INFO_TEXT)
+        add_space, true_rows, true_cols = self.__get_true_size()
 
         # check for the minimal screen size
         rows_cond = curses.LINES < self.__MIN_ROWS
-        cols_cond = curses.COLS - max_add_len < self.__MIN_COLS
+        cols_cond = curses.COLS - add_space < self.__MIN_COLS
         if rows_cond or cols_cond:
             return False
 
-        # number of the screen's rows == (minimum of rows) - (2*border field)
-        map_rows = self.__MIN_ROWS - 2
-        # number of the screen's columns == (terminal's current number of
-        # the columns) - (size of address info - 2*border field)
-        map_cols = curses.COLS - max_add_len - 2
-
         # creating the heap map screen decomposition
-        decomposition = self.create_screen_decomposition(map_rows, map_cols)
+        decomposition = self.create_screen_decomposition(true_rows, true_cols)
         assert decomposition
 
         # printing heap map decomposition to the console window
         self.matrix_print(decomposition, self.__MIN_ROWS, curses.COLS,
-                          max_add_len)
+                          add_space)
         # printing heap info to the console window
-        self.info_print(max_add_len)
+        self.info_print(add_space)
         # update map information and coordinates
-        self.__map_cords.update({'row': 1, 'col': max_add_len + 1,
-                                 'map': decomposition})
+        self.__map_cords = {'row': 1, 'col': add_space + 1,
+                            'map': decomposition}
         return True
 
     def __set_current_snap(self, following_snap):
@@ -469,7 +484,7 @@ class HeapMapVisualization(object):
         Arguments:
             following_snap(int): number of the snapshot to set
         """
-        if following_snap in range(1, self.__heap['max'] + 1):
+        if following_snap in range(1, len(self.__heap['snapshots']) + 1):
             self.__current_snap = following_snap
             return True
         else:
@@ -542,7 +557,7 @@ class HeapMapVisualization(object):
             else:
                 self.__window.move(self.__map_cords['row'], row_col[1])
 
-    def print_field_info(self):
+    def print_field_info(self, is_heat=False):
         """ Prints information about memory space pointed by the cursor """
         if not self.__map_cords:
             return
@@ -560,16 +575,48 @@ class HeapMapVisualization(object):
 
         try:
             data = self.__map_cords['map']['data'][matrix_row][matrix_col]
-            if data['uid'] is None:
+            if is_heat:
+                self.__print_heat_stats(data)
+            elif data['uid'] is None:
                 self.__print_glob_stats(data['address'])
             else:
                 self.__print_alloc_stats(data)
         except KeyError:
             pass
         # printing menu to the console window, was also cleared
-        self.menu_print(self.__MENU_TEXT)
+        if is_heat:
+            self.menu_print(self.__HEAT_MENU_TEXT)
+        else:
+            self.menu_print(self.__MENU_TEXT)
         # move cursor to the original position
         self.__window.move(*orig_row_col)
+
+    def __print_heat_stats(self, data):
+        heat_text = 'HEAT INFO:'
+        access_text = "Number of accesses: "
+        address_text = "Starting address: "
+        margin = len(access_text)
+
+        row_pos = self.__map_cords['map']['rows'] + 2
+        col_pos = (curses.COLS - len(heat_text)) // 2
+
+        self.__window.hline(self.BORDER_SYM, curses.COLS,
+                            curses.color_pair(self.__colors.info_text))
+        self.__window.addstr(row_pos, col_pos, heat_text,
+                             curses.color_pair(self.__colors.info_text))
+
+        # 1st line
+        text1 = access_text + ' '*(margin - len(access_text))
+        text1 += str(data['access']) + 'x'
+        text1 += ' '*(curses.COLS - len(text1))
+        # 2nd line
+        text2 = address_text + ' '*(margin - len(address_text))
+        text2 += str(int(data['address']))
+        text2 += ' '*(curses.COLS - len(text2))
+
+        # print current field's information
+        self.__window.addstr(row_pos + 1, 0, text1 + text2,
+                             curses.color_pair(self.__colors.info_text))
 
     def __print_alloc_stats(self, data):
         """ Prints allocation info
@@ -652,6 +699,84 @@ class HeapMapVisualization(object):
         self.__window.addstr(row_pos + 1, 0, text1 + text2 + text3,
                              curses.color_pair(self.__colors.info_text))
 
+    def __get_true_size(self):
+        # calculate space for the addresses information
+        max_add_len = len(str(self.__heap['stats']['max_address']))
+        if max_add_len < len(self.__ADDRESS_INFO_TEXT):
+            max_add_len = len(self.__ADDRESS_INFO_TEXT)
+
+        # number of the screen's rows == (minimum of rows) - (2*border field)
+        map_rows = self.__MIN_ROWS - 2
+        # number of the screen's columns == (terminal's current number of
+        # the columns) - (size of address info - 2*border field)
+        map_cols = curses.COLS - max_add_len - 2
+
+        return max_add_len, map_rows, map_cols
+
+    def create_heat_decomposition(self, rows, cols):
+        """ Create a matrix with corresponding HEAT representation
+
+        Arguments:
+            rows(int): total number of the screen's rows
+            cols(int): total number of the screen's columns
+
+        Returns:
+            dict: matrix representing HEAT decomposition
+                  and size of the field
+        """
+        field_size = self.__calculate_field_size(rows, cols)
+
+        # creating empty matrix
+        matrix = [[None for _ in range(cols)] for _ in range(rows)]
+
+        accesses = self.__heap['map']
+        # set starting address to first approx field
+        min_address = self.__heap['stats']['min_address']
+        curr_add = min_address
+        for row in range(rows):
+            for col in range(cols):
+
+                access_number = max(accesses[int(curr_add) + i - min_address]
+                                    for i in range(int(field_size)))
+
+                matrix[row][col] = {"access": access_number,
+                                    "address": curr_add
+                                    }
+
+                curr_add += field_size
+
+        return {"data": matrix, "field_size": field_size,
+                "rows": rows, "cols": cols}
+
+    def heat_map_draw(self):
+        """ Draw the heat map screen """
+        curses.update_lines_cols()
+        self.__window.clear()
+        self.__map_cords = None
+
+        add_space, true_rows, true_cols = self.__get_true_size()
+
+        # check for the minimal screen size
+        rows_cond = curses.LINES < self.__MIN_ROWS
+        cols_cond = curses.COLS - add_space < self.__MIN_COLS
+        if rows_cond or cols_cond:
+            self.resize_req_print()
+            return
+
+        # creating the heat map screen decomposition
+        decomposition = self.create_heat_decomposition(true_rows, true_cols)
+        assert decomposition
+
+        # printing heat map decomposition to the console window
+        self.matrix_print(decomposition, self.__MIN_ROWS, curses.COLS,
+                          add_space)
+
+        self.menu_print(self.__HEAT_MENU_TEXT)
+
+        self.__map_cords = {'row': 1, 'col': add_space + 1,
+                            'map': decomposition}
+        self.cursor_reset()
+
     def __init__(self, window, heap):
         """ Initialize the HEAP MAP visualization object
 
@@ -666,7 +791,7 @@ class HeapMapVisualization(object):
         # currently printed map's snapshot
         self.__current_snap = 0
         # heap map's metadata and coordinates
-        self.__map_cords = {'row': 0, 'col': 0, 'map': {}}
+        self.__map_cords = None
         # instance of the color module object
         self.__colors = HeapMapColors(HeapMapColors.CURSES_COLORS)
 
@@ -674,12 +799,42 @@ class HeapMapVisualization(object):
         curses.curs_set(2)
 
 
-def heap_map_prompt(window, heap):
+def heat_mode(window, heat):
+    """ HEAT visualization prompt
+
+    Arguments:
+        window(any): initialized curses window
+        heat(dict): the heat map representation
+    """
+    # instantiate of the heat map visualization object
+    vis_obj = HeapMapVisualization(window, heat)
+
+    vis_obj.heat_map_draw()
+    vis_obj.print_field_info(True)
+    while True:
+        # catching key value
+        key = window.getch()
+
+        # quit of the visualization
+        if key in (ord('q'), ord('Q')):
+            break
+        # cursor moved
+        elif key in (ord('4'), ord('6'), ord('8'), ord('5')):
+            vis_obj.cursor_move(key)
+            vis_obj.print_field_info(True)
+        # change of the screen size occurred
+        elif key == curses.KEY_RESIZE:
+            vis_obj.heat_map_draw()
+            vis_obj.print_field_info(True)
+
+
+def heap_map_prompt(window, heap, heat):
     """ HEAP visualization prompt
 
     Arguments:
         window(any): initialized curses window
-        heap(dict): the heap representation
+        heap(dict): the heap map representation
+        heat(dict): the heat map representation
     """
     # instantiate of the heap map visualization object
     vis_obj = HeapMapVisualization(window, heap)
@@ -709,12 +864,16 @@ def heap_map_prompt(window, heap):
         elif key in (ord('4'), ord('6'), ord('8'), ord('5')):
             vis_obj.cursor_move(key)
             vis_obj.print_field_info()
+        # HEAT map
+        elif key in (ord('h'), ord('H')):
+            heat_mode(window, heat)
+            vis_obj.following_snapshot(vis_obj.CURRENT_SNAPSHOT)
         # change of the screen size occurred
         elif key == curses.KEY_RESIZE:
             vis_obj.following_snapshot(vis_obj.CURRENT_SNAPSHOT)
 
 
-def heap_map(heap):
+def heap_map(heap, heat):
     """ Call curses wrapper and start visualization
 
         Wrapper initialize terminal,
@@ -732,13 +891,15 @@ def heap_map(heap):
     # after integration remove try block
     try:
         # call heap map visualization prompt in curses wrapper
-        curses.wrapper(heap_map_prompt, heap)
+        curses.wrapper(heap_map_prompt, heap, heat)
     except curses.error as error:
         print('Screen too small!', file=sys.stderr)
         print(str(error), file=sys.stderr)
 
 
 if __name__ == "__main__":
-    with open("memory.perf") as prof_json:
-        prof = heap_representation.create(json.load(prof_json))
-        heap_map(prof)
+    with open("memory.perf") as f:
+        heap = heap_representation.create_heap_map(json.load(f))
+    with open("memory.perf") as f:
+        heat = heap_representation.create_heat_map(json.load(f))
+    heap_map(heap, heat)
--- perun/view/memory/cli/heap_representation.py
+++ perun/view/memory/cli/heap_representation.py
@@ -2,7 +2,7 @@
 __author__ = 'Radim Podola'
 
 
-def create(profile):
+def create_heap_map(profile):
     """ Create the heap map representation for visualization
     Arguments:
         profile(dict): the memory profile
@@ -11,7 +11,7 @@ def create(profile):
         dict: the heap map representation
 
     Format of the heap map representation is following:
-        {"max": number of snapshots taken (int),
+        {"type": type of representation (heap/heat)
          "unit": used memory unit (string),
          "stats": { # same stats like for each snapshots but calculated
                     # over all the snapshots
@@ -46,9 +46,9 @@ def create(profile):
     """
     snapshots = [a for a in profile['snapshots']]
 
-    # modifying the memory profile to __heap map representation
+    # modifying the memory profile to heap map representation
     for snap in snapshots:
-        snap['map'] = get_map(snap['resources'])
+        snap['map'] = get_heap_map(snap['resources'])
         del snap['resources']
 
     # calculating existing allocations for each snapshot
@@ -56,14 +56,77 @@ def create(profile):
     # adding statistics about each snapshot
     glob_stats = add_stats(snapshots)
 
-    return {'snapshots': snapshots,
+    return {'type': 'heap',
+            'snapshots': snapshots,
             'info': chunks,
-            'max': len(snapshots),
             'stats': glob_stats,
             'unit': profile['header']['units']['memory']}
 
 
-def get_map(resources):
+def create_heat_map(profile):
+    """ Create the heat map representation for visualization
+    Arguments:
+        profile(dict): the memory profile
+
+    Returns:
+        dict: the heat map representation
+
+    Format of the heat map representation is following:
+        {"type": type of representation (heap/heat)
+         "unit": used memory unit (string),
+         "stats": {"max_address": maximal address of the allocated memory
+                                  in snapshot(int),
+                   "min_address": minimal address of the allocated memory
+                                  in snapshot (int)
+                  },
+         "map": [# mapping all the allocations to number of access to address
+                 # size of array == max_address - min_address
+                    0, 0, 5
+                ]
+        }
+    """
+    resources = []
+    for snap in profile['snapshots']:
+        resources.extend(snap['resources'])
+
+    # adding statistics
+    max_address = max(item.get('address', 0) + item.get('amount', 0)
+                                      for item in resources)
+    min_address = min(item.get('address', 0) for item in resources)
+
+    # transform the memory profile to heat map representation
+    map = get_heat_map(resources, min_address, max_address)
+
+    return {"type": 'heat',
+            "stats": {'max_address': max_address,
+                      'min_address': min_address,
+                      },
+            "map": map,
+            'unit': profile['header']['units']['memory']}
+
+
+def get_heat_map(resources, min_add, max_add):
+    """ Parse resources from the memory profile to HEAT map representation
+    Arguments:
+        resources(list): list of the resources from the memory profile
+
+    Returns:
+        list: list of the number of access to address
+    """
+    address_count = max_add - min_add
+    add_map = [0 for _ in range(address_count)]
+
+    for res in resources:
+        address = res['address']
+        amount = res['amount']
+
+        for add in range(amount):
+            add_map[address + add - min_add] += 1
+
+    return add_map
+
+
+def get_heap_map(resources):
     """ Parse resources from the memory profile to simpler representation
     Arguments:
         resources(list): list of the resources from the memory profile
