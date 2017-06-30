1839552a6a073474b1edfa5d2fed142d067e4c78
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2017-06-30 22:30:10 +0200

    Restyle the graphs a little

--- perun/utils/bokeh_helpers.py
+++ perun/utils/bokeh_helpers.py
@@ -8,8 +8,9 @@ import perun.utils.log as log
 
 __author__ = 'Tomas Fiedor'
 
-GRAPH_LR_PADDING = 50
-GRAPH_TB_PADDING = 100
+GRAPH_LR_PADDING = 0
+GRAPH_B_PADDING = 100
+GRAPH_T_PADDING = 50
 
 
 def get_unique_colours_for_(data_source, key):
@@ -39,9 +40,29 @@ def configure_axis(axis, axis_title):
         axis(any): Bokeh plot's axis object
         axis_title(str): title of the axis
     """
-    axis.axis_label_text_font_style = 'italic'
-    axis.axis_label_text_font_size = '12pt'
+    axis.axis_label_text_font = 'helvetica'
+    axis.axis_label_text_font_style = 'bold'
+    axis.axis_label_text_font_size = '14pt'
     axis.axis_label = axis_title
+    axis.major_label_text_font_style = 'bold'
+    axis.major_tick_line_width = 2
+    axis.minor_tick_line_width = 2
+    axis.major_tick_in = 8
+    axis.major_tick_out = 8
+    axis.minor_tick_in = 4
+    axis.minor_tick_out = 4
+
+
+def configure_grid(grid):
+    """Sets the given grid
+
+    Arguments:
+        grid(bokeh.Grid): either x or y grid
+    """
+    grid.minor_grid_line_color = 'grey'
+    grid.minor_grid_line_alpha = 0.2
+    grid.grid_line_color = 'grey'
+    grid.grid_line_alpha = 0.4
 
 
 def configure_title(graph_title, title):
@@ -53,7 +74,7 @@ def configure_title(graph_title, title):
     """
     graph_title.text_font = 'helvetica'
     graph_title.text_font_style = 'bold'
-    graph_title.text_font_size = '14pt'
+    graph_title.text_font_size = '21pt'
     graph_title.align = 'center'
     graph_title.text = title
 
@@ -68,8 +89,27 @@ def configure_graph_canvas(graph, graph_width):
     graph.width = graph_width
     graph.min_border_left = GRAPH_LR_PADDING
     graph.min_border_right = GRAPH_LR_PADDING
-    graph.min_border_top = GRAPH_TB_PADDING
-    graph.min_border_bottom = GRAPH_TB_PADDING
+    graph.min_border_top = GRAPH_T_PADDING
+    graph.min_border_bottom = GRAPH_B_PADDING
+    graph.outline_line_width = 2
+    graph.outline_line_color = 'black'
+
+    for r in graph.renderers:
+        if hasattr(r, 'glyph'):
+            r.glyph.line_width = 2
+            r.glyph.line_color = 'black'
+
+
+def configure_legend(graph):
+    """
+    Arguments:
+        graph(bokeh.Figure): bokeh graph for which we will configure the legend
+    """
+    graph.legend.border_line_color = 'black'
+    graph.legend.border_line_width = 2
+    graph.legend.border_line_alpha = 1.0
+    graph.legend.label_text_font_style = 'bold'
+    graph.legend.location = 'top_right'
 
 
 def configure_graph(graph, profile, func, graph_title, x_axis_label, y_axis_label, graph_width):
@@ -89,6 +129,9 @@ def configure_graph(graph, profile, func, graph_title, x_axis_label, y_axis_labe
     configure_axis(graph.xaxis, x_axis_label)
     configure_axis(graph.yaxis, y_axis_label)
     configure_title(graph.title, graph_title)
+    configure_legend(graph)
+    configure_grid(graph.ygrid)
+    configure_grid(graph.grid)
 
     # If of key is ammount, add unit
     if func not in ('count', 'nunique') and not y_axis_label.endswith("]"):
