2b196694ab795bf98d1a8cf744abdd58d1e43378
Radim Podola <RPodola@seznam.cz>
2017-07-02 11:33:58 +0200

    Add Flame graph visualization using 3rd party visualizer by Brendan Gregg.

---
+++ perun/view/flamegraph/run.py
@@ -0,0 +1,19 @@
+"""Flame graph visualization of the profiles."""
+
+import click
+import perun.view.flamegraph.flamegraph as flame
+from perun.utils.helpers import pass_profile
+
+__author__ = 'Radim Podola'
+
+
+@click.command()
+@click.option('--filename', '-f', default="flame.svg",
+              help="Outputs the graph to file specified by filename.")
+@click.option('--graph-height', '-h', default=20,
+              help="Changes the width of the generated Graph.")
+@pass_profile
+# Fixme: Consider breaking this to two
+def flamegraph(profile, filename, graph_height, **_):
+    """Flame graph visualization of the profile."""
+    flame.draw_flame_graph(profile, filename, graph_height)
