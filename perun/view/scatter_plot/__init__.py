c881b8fe87a143bbfb0eb291ae5916680872cc1d
Jiří Pavela <xpavel32@stud.fit.vutbr.cz>
2017-09-21 23:12:42 +0200

    Remove scatter-plot in-progress module which was included due to bad branching of query-extension.
    scatter_plot/: removed, will be included in different branch

--- perun/view/scatter_plot/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Scatter plot creates graph with dots representing measured values.
-
-Scatter plot also support plotting of regression analysis outputs (i.e. models).
-"""
-
-SUPPORTED_PROFILES = ['mixed']
-
-__author__ = 'Jiri Pavela'
