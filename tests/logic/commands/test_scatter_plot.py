75b6a423841f3b939e041d4db71ba9a312068e6c
Jiří Pavela <xpavel32@stud.fit.vutbr.cz>
2017-09-28 23:02:23 +0200

    Add tests for the new scatter plot (beta).
    conftest.py: add one new fixture
    postprocess_profiles/: add new postprocess profiles with models from different methods
    test_scatter_plot.py: 4 new tests covering a) models plotting, b) points plotting, c) cli control d) cli invalid inputs

---
+++ tests/logic/commands/test_scatter_plot.py
@@ -0,0 +1,109 @@
+""" Basic tests for scatter plot visualization """
+
+import os
+
+from click.testing import CliRunner
+
+import perun.cli as cli
+import perun.view.scatter_plot.factory as scatter
+
+__author__ = 'Jiri Pavela'
+
+
+def test_scatter_plot_models(postprocess_profiles):
+    """ Test the scatter plot on complexity profiles with models.
+
+    Expecting no errors or exceptions.
+    """
+    # Filter the postprocess profiles
+    tested_profiles = [p for p in list(postprocess_profiles) if 'computation' in p[0]]
+    assert len(tested_profiles) == 5
+
+    for profile in tested_profiles:
+        # Create graphs from one profile
+        graphs = scatter.create_from_params(profile[1], 'amount', 'structure-unit-size',
+                                            'structure-unit-size', 'amount [us]',
+                                            "Plot of 'amount' per 'structure-unit-size'")
+        results = list(graphs)
+
+        # Check if scatter plot generated expected amount of graphs for each profile
+        if ('full_computation.perf' in profile[0] or 'initial_guess_computation.perf' in profile[0]
+                or 'iterative_computation.perf' in profile[0]):
+            assert len(results) == 2
+        elif 'bisection_computation.perf' in profile[0]:
+            assert len(results) == 4
+        elif 'interval_computation.perf' in profile[0]:
+            assert len(results) == 6
+        else:
+            assert False
+
+
+def test_scatter_plot_no_models(full_profiles):
+    """ Test the scatter plot on complexity profiles without models.
+
+    Expecting no errors or exceptions.
+    """
+    # Filter the full profiles, only the complexity one is needed
+    complexity_prof = [p for p in list(full_profiles) if 'prof-2-2017' in p[0]]
+    assert len(complexity_prof) == 1
+    profile = complexity_prof[0]
+
+    # Create graphs from one profile without models
+    graphs = scatter.create_from_params(profile[1], 'amount', 'structure-unit-size',
+                                        'structure-unit-size', 'amount [us]',
+                                        "Plot of 'amount' per 'structure-unit-size'")
+    results = list(graphs)
+
+    # Graphs for two functions should be generated
+    assert len(results) == 2
+
+
+def test_scatter_plot_cli(pcs_full, postprocess_profiles):
+    """ Test creating bokeh scatter plot from the cli
+
+    Expecting no errors and created scatter_plot_result0.html, scatter_plot_result1.html files
+    """
+    # Filter the postprocess profiles, test only on the full computation
+    tested_profiles = [p for p in list(postprocess_profiles) if 'full_computation' in p[0]]
+    assert len(tested_profiles) == 1
+    profile = tested_profiles[0]
+
+    # Run the cli on the given profile
+    runner = CliRunner()
+    result = runner.invoke(cli.show, [profile[0], 'scatter_plot', '--of=amount',
+                                      '--per=structure-unit-size', '--filename=scatter_plot',
+                                      '-xl=structure-unit-size', '-yl=amount [us]'])
+
+    assert result.exit_code == 0
+    assert 'scatter_plot_result0.html' in os.listdir(os.getcwd())
+    assert 'scatter_plot_result1.html' in os.listdir(os.getcwd())
+
+
+def test_scatter_plot_cli_errors(pcs_full, postprocess_profiles):
+    """ Test creating bokeh scatter plot from the cli with invalid inputs
+
+    Expecting to fail all commands and not create any graph files.
+    """
+    # Filter the postprocess profiles, test only on the full computation
+    tested_profiles = [p for p in list(postprocess_profiles) if 'full_computation' in p[0]]
+    assert len(tested_profiles) == 1
+    profile = tested_profiles[0]
+
+    runner = CliRunner()
+    # Try invalid view argument
+    result = runner.invoke(cli.show, [profile[0], 'scatter', '--of=amount',
+                                      '--per=structure-unit-size'])
+    assert result.exit_code == 2
+    assert 'No such command "scatter"' in result.output
+
+    # Try invalid --of value
+    result = runner.invoke(cli.show, [profile[0], 'scatter_plot', '--of=amou',
+                                      '--per=structure-unit-size'])
+    assert result.exit_code == 2
+    assert 'invalid choice: amou' in result.output
+
+    # Try invalid --per value
+    result = runner.invoke(cli.show, [profile[0], 'scatter_plot', '--of=amount',
+                                      '--per=struct'])
+    assert result.exit_code == 2
+    assert 'invalid choice: struct' in result.output
