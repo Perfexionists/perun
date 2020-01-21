4d8ca8314c4690aa054a1c9a6a1a76a117143a43
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2020-01-21 18:13:42 +0100

    Break table view to two modes
    table: break to view of resources and tables.

--- perun/view/table/run.py
+++ perun/view/table/run.py
@@ -7,26 +7,25 @@ import perun.profile.convert as convert
 from perun.profile.factory import pass_profile
 
 
-def create_table_from(profile):
+def create_table_from(profile, conversion_function):
     """Using the tabulate package, transforms the profile into table.
 
     Currently, the represention contains all of the possible keys.
 
     :param dict profile: profile transformed into the table
+    :param function conversion_function: function that converts profile to table
     :return: tabular representation of the profile in string
     """
-    dataframe = convert.resources_to_pandas_dataframe(profile)
+    dataframe = conversion_function(profile)
     headers = list(dataframe)
     resource_table = dataframe.values.tolist()
     return tabulate.tabulate(resource_table, headers=headers)
 
 
-@click.command()
+@click.group()
 @pass_profile
-def table(profile, **kwargs):
-    """Textual representation of the resources as a table.
-
-    Table shows resources in classical tabular format.
+def table(_, **__):
+    """Textual representation of the profile as a table.
 
     .. _tabulate: https://pypi.org/project/tabulate/
 
@@ -35,8 +34,8 @@ def table(profile, **kwargs):
       * **Interpretation style**: textual
       * **Visualization backend**: tabulate_
 
-    The table is formated using the tabulate_ library. Currently, we support only the simplest form,
-    and allow output to file.
+    The table is formatted using the tabulate_ library. Currently, we support only the simplest
+    form, and allow output to file.
 
     Refer to :ref:`views-table` for more thorough description and example of
     `table` interpretation possibilities.
@@ -46,5 +45,20 @@ def table(profile, **kwargs):
     TODO: Add format
     TODO: Add output to file
     """
-    profile_as_table = create_table_from(profile)
+    pass
+
+
+@table.command()
+@pass_profile
+def resources(profile, **_):
+    """Outputs the resources of the profile as a table"""
+    profile_as_table = create_table_from(profile, convert.resources_to_pandas_dataframe)
+    print(profile_as_table)
+
+
+@table.command()
+@pass_profile
+def models(profile, **_):
+    """Outputs the models of the profile as a table"""
+    profile_as_table = create_table_from(profile, convert.models_to_pandas_dataframe)
     print(profile_as_table)
--- tests/test_table.py
+++ tests/test_table.py
@@ -4,18 +4,49 @@ import os
 from click.testing import CliRunner
 
 import perun.cli as cli
+import perun.vcs as vcs
 
 TABLE_TEST_DIR = os.path.join(os.path.split(__file__)[0], "table_files")
 __author__ = 'Tomas Fiedor'
 
 
-def test_table_cli(pcs_full):
+def profile_filter(generator, rule):
+    """Finds concrete profile by the rule in profile generator.
+
+    Arguments:
+        generator(generator): stream of profiles as tuple: (name, dict)
+        rule(str): string to search in the name
+
+    Returns:
+        Profile: first profile with name containing the rule
+    """
+    # Loop the generator and test the rule
+    for profile in generator:
+        if rule in profile[0]:
+            return profile[0]
+
+
+def test_table_cli(helpers, pcs_full, postprocess_profiles):
     """Test outputing profiles as tables"""
     runner = CliRunner()
     result = runner.invoke(cli.show, [
-        '0@i', 'table'
+        '0@i', 'table', 'resources'
     ])
     assert result.exit_code == 0
-    with open(os.path.join(TABLE_TEST_DIR, 'table_ref_basic'), 'r') as trb:
+    with open(os.path.join(TABLE_TEST_DIR, 'table_resources_ref_basic'), 'r') as trb:
         assert sorted(result.output) == sorted("".join(trb.readlines()))
 
+    models_profile = profile_filter(postprocess_profiles, 'complexity-models.perf')
+    added = helpers.prepare_profile(
+        pcs_full.get_job_directory(), models_profile, vcs.get_minor_head()
+    )
+    result = runner.invoke(cli.add, ['--keep-profile', '{}'.format(added)])
+    assert result.exit_code == 0
+
+    result = runner.invoke(cli.show, [
+        '0@i', 'table', 'models'
+    ])
+    assert result.exit_code == 0
+    print(result.output)
+    with open(os.path.join(TABLE_TEST_DIR, 'table_models_ref_basic'), 'r') as trb:
+        assert sorted(result.output) == sorted("".join(trb.readlines()))
