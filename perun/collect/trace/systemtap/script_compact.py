e5fd030a356393961bf543d7660dd69da9e6ae6d
JiriPavela <xpavel32@stud.fit.vutbr.cz>
2021-01-15 20:42:50 +0100

    Fix Dynamic Sampling bug.

--- perun/collect/trace/systemtap/script_compact.py
+++ perun/collect/trace/systemtap/script_compact.py
@@ -80,6 +80,7 @@ ENTRY_APPROX_SAMPLE_TEMPLATE = """
         {sampling_flag}[tid, pname] ++
         {{probe_handler}}
     }}}}
+    {sampling_cnt}[tid, pname] ++
 """.format(
     sampling_cnt=ARRAY_SAMPLE_COUNTER,
     sampling_thr=ARRAY_SAMPLE_THRESHOLD,
