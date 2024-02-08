4a9f1fff6965f91d017058d004460adacafbeec0
Tomas Fiedor <TomasFiedor@gmail.com>
2024-02-08 09:13:34 +0100

    Blackify the code

--- perun/collect/ktrace/run.py
+++ perun/collect/ktrace/run.py
@@ -19,6 +19,7 @@ from perun.collect.ktrace import symbols, bpfgen, interpret
 
 BUSY_WAIT: int = 5
 
+
 def get_kernel():
     """Returns the identification of the kernel
 
@@ -66,36 +67,36 @@ def collect(**kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
     :param kwargs: stash of shared values between the phases
     :return: collection status (error or OK), error message (if error happened) and shared parameters
     """
-    ktrace_coloured = log.in_color("ktrace", color='yellow', attribute_style=['bold'])
+    ktrace_coloured = log.in_color("ktrace", color="yellow", attribute_style=["bold"])
     cmd_coloured = log.in_color(f"{kwargs['cmd_name']}")
 
     # First we wait for starting the ktrace
     log.info(f"You can now run the '{ktrace_coloured}' (yourself).")
-    log.info(f"Waiting for {ktrace_coloured} to start", end='')
+    log.info(f"Waiting for {ktrace_coloured} to start", end="")
     while True:
-        log.info('.', end='')
+        log.info(".", end="")
 
         if processes.is_process_running("ktrace"):
-            log.info('')
+            log.info("")
             break
         time.sleep(BUSY_WAIT)
 
-    log.info(f"{ktrace_coloured}: ", end='')
+    log.info(f"{ktrace_coloured}: ", end="")
     log.tag("running", "green")
 
     log.info("")
     log.info(f"You can now run the '{cmd_coloured}' (yourself).")
-    log.info(f"Waiting for {ktrace_coloured} to finish profiling '{cmd_coloured}'", end='')
+    log.info(f"Waiting for {ktrace_coloured} to finish profiling '{cmd_coloured}'", end="")
 
     while True:
-        log.info('.', end='')
+        log.info(".", end="")
 
         if not processes.is_process_running("ktrace"):
-            log.info('')
+            log.info("")
             break
         time.sleep(BUSY_WAIT)
 
-    log.info(f"Collection of '{cmd_coloured}'", end='')
+    log.info(f"Collection of '{cmd_coloured}'", end="")
     log.done()
 
     return CollectStatus.OK, "", dict(kwargs)
@@ -108,11 +109,15 @@ def after(**kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
     raw_data_file = Path(Path(__file__).resolve().parent, "bpf_build", "output.log")
     output_file = Path(Path(__file__).resolve().parent, "bpf_build", "profile.csv")
 
-    parsed_traces = interpret.parse_traces(raw_data_file, kwargs["idx_to_func"], interpret.FuncDataDetails)
+    parsed_traces = interpret.parse_traces(
+        raw_data_file, kwargs["idx_to_func"], interpret.FuncDataDetails
+    )
     trace_data = interpret.traces_details_to_pandas(parsed_traces)
     trace_data.to_csv(output_file, index=False)
 
-    log.info(f"Saving traces to {log.in_color('tracedata.pickle', color='grey', attribute_style=['bold'])}")
+    log.info(
+        f"Saving traces to {log.in_color('tracedata.pickle', color='grey', attribute_style=['bold'])}"
+    )
     with open("tracedata.pickle", "wb") as pickle_file:
         pickle.dump(parsed_traces, pickle_file)
 
@@ -121,12 +126,7 @@ def after(**kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
     )
 
     resources = interpret.trace_details_to_resources(parsed_traces)
-    kwargs['profile'] = {
-        'global': {
-            'time': parsed_traces.total_runtime,
-            'resources': resources
-        }
-    }
+    kwargs["profile"] = {"global": {"time": parsed_traces.total_runtime, "resources": resources}}
 
     return CollectStatus.OK, "", dict(kwargs)
 
