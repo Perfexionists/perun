42e71f72644d0415e164c3df0eb4d4d2848f9531
Tomas Fiedor <TomasFiedor@gmail.com>
2024-03-01 17:49:03 +0100

    Create basic implementation of kperf

---
+++ perun/collect/kperf/parser.py
@@ -0,0 +1,39 @@
+"""
+Contains helper functions for parsing information from perf
+"""
+from __future__ import annotations
+
+# Standard Imports
+from typing import Any
+
+# Third-Party Imports
+
+# Perun Imports
+
+
+def parse_events(perf_events: str) -> list[dict[str, Any]]:
+    """Parses perf events into a list of resources
+
+    Each resource is identified by its topmost called function (uid),
+    and contains traces and unit (the bottom function). For each
+    function we count the number of samples.
+
+    :param perf_events: string with perf events
+    :return: list of resources
+    """
+    resources = []
+    for event in perf_events.split("\n"):
+        if not event.strip():
+            continue
+        record, samples = event.split(" ")
+        parts = record.split(";")
+        command, trace, uid = parts[0], parts[1:-1], parts[-1]
+        resources.append(
+            {
+                "amount": samples,
+                "uid": uid,
+                "command": command,
+                "trace": [{"func": f} for f in trace],
+            }
+        )
+    return resources
--- perun/collect/kperf/run.py
+++ perun/collect/kperf/run.py
@@ -2,30 +2,93 @@
 from __future__ import annotations
 
 # Standard Imports
+import os
+import subprocess
+from pathlib import Path
 from typing import Any
 
 # Third-Party Imports
 import click
 
 # Perun Imports
+from perun.collect.kperf import parser
 from perun.logic import runner
 from perun.utils import log
 from perun.utils.structs import Executable, CollectStatus
+from perun.utils.external import commands
 
 
-def before(**kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
+def before(**_: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
+    """Checks that all dependencies are runnable"""
     log.major_info("Checking for Dependencies")
+    # Check that perf can be run
+    all_found = True
+    if commands.is_executable("perf --help"):
+        log.minor_success(f"{log.cmd_style('perf')}", "executable")
+    else:
+        all_found = False
+        log.minor_fail(f"{log.cmd_style('perf')}", "not-executable")
+
+    # Check that helper script can be run
+    script_dir = Path(Path(__file__).resolve().parent, "scripts")
+    parse_script = os.path.join(script_dir, "stackcollapse-perf.pl")
+    if commands.is_executable(f'echo "" | {parse_script}'):
+        log.minor_success(f"{log.cmd_style(parse_script)}", "executable")
+    else:
+        all_found = False
+        log.minor_fail(f"{log.cmd_style(parse_script)}", "not-executable")
+
+    if not all_found:
+        log.minor_fail("Checking dependencies")
+        return CollectStatus.ERROR, "Some depedencies cannot be run", {}
+    else:
+        log.minor_success("Checking dependencies")
+
     return CollectStatus.OK, "", {}
 
 
-def collect(executable: Executable, **_: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
+def collect(executable: Executable, **kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
+    """Runs the workload with perf and transforms it to stack traces"""
     log.major_info("Collecting performance data")
-    return CollectStatus.OK, "", {}
+    script_dir = Path(Path(__file__).resolve().parent, "scripts")
+    parse_script = os.path.join(script_dir, "stackcollapse-perf.pl")
+
+    if kwargs.get("with_sudo", False):
+        perf_record_command = f"perf record -q -g -o collected.data {executable}"
+        perf_script_command = f"perf script -i collected.data | {parse_script}"
+    else:
+        perf_record_command = f"sudo perf record -q -g -o collected.data {executable}"
+        perf_script_command = f"sudo perf script -i collected.data | {parse_script}"
+
+    try:
+        commands.run_safely_external_command(perf_record_command)
+        out, err = commands.run_safely_external_command(perf_script_command)
+        log.minor_success(f"Raw data from {log.cmd_style(executable)}", "collected")
+    except subprocess.CalledProcessError:
+        log.minor_fail(f"Raw data from {log.cmd_style(executable)}", "not collected")
+        return CollectStatus.ERROR, "Command failed", {}
+    kwargs["raw_data"] = out.decode("utf-8")
+    return CollectStatus.OK, "", kwargs
 
 
 def after(**kwargs: Any) -> tuple[CollectStatus, str, dict[str, Any]]:
+    """Parses the raw data into performance profile"""
     log.major_info("Creating performance profile")
-    return CollectStatus.OK, "", {}
+    resources = parser.parse_events(kwargs["raw_data"])
+
+    if resources:
+        log.minor_success("perf events", "parsed")
+    else:
+        log.warn("possibly empty raw data: no resources were parsed")
+        return CollectStatus.OK, "", {}
+
+    kwargs["profile"] = {
+        "global": {
+            "time": "TODO",
+            "resources": resources,
+        }
+    }
+    return CollectStatus.OK, "", kwargs
 
 
 @click.command()
