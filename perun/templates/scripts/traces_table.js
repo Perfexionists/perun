fa4ba0db556e1f62eb0f0951a3b04ea4a3ae5d47
Robert Vašek <robert.vashek@gmail.com>
2026-07-02 11:49:51 +0200

    FEAT: add call stack depth column to traces table and implement copy-to-clipboard functionality for function names in modal

--- perun/templates/scripts/traces_table.js
+++ perun/templates/scripts/traces_table.js
@@ -7,7 +7,8 @@ const TRACES_TOOLTIPS = {
     target: 'The amount of resources consumed by the Unit/Trace in the target profile.',
     prop_rel_delta: 'The difference between the relative resource consumption proportionally to the total baseline and target consumption change. For example, if the baseline and target consumed 2M and 1M CPU cycles in total, respectively, and a function \'foo\' consumed 100K cycles in both cases, the proportional difference is +5% as \'foo\' now consumes 10% total resources up from 5%.',
     abs_delta: 'The difference of Target - Baseline resource consumption. For example, if the baseline and target consumed 2M and 1M CPU cycles in total, respectively, and a function \'foo\' consumed 100K and 75K cycles in baseline, resp. target, the absolute difference is -25K.',
-    rel_delta: 'The difference of Target - Baseline resource consumption in relative terms. For example, if the baseline and target consumed 2M and 1M CPU cycles in total, respectively, and a function \'foo\' consumed 100K and 80K cycles in baseline, resp. target, the relative difference is -20%.'
+    rel_delta: 'The difference of Target - Baseline resource consumption in relative terms. For example, if the baseline and target consumed 2M and 1M CPU cycles in total, respectively, and a function \'foo\' consumed 100K and 80K cycles in baseline, resp. target, the relative difference is -20%.',
+    depth: 'Number of frames in the call stack for this trace.'
 };
 
 class TracesTable {
